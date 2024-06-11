548e45fdae1e3f5e9060d465b035c814033e21f7
Mostafa Elhoushi <melhoushi@meta.com>
2024-06-11 04:39:54 +0000

    get the eval.py to somehow be fast

--- eval.py
+++ eval.py
@@ -1,19 +1,36 @@
-import lm_eval
 import torch
-import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TextStreamer
+import lm_eval
 
 from any4 import convert, any4
+# device = "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model_name = "meta-llama/Meta-Llama-3-8B"
+model_name = "meta-llama/Llama-2-7b-hf"
+# model_name = "meta-llama/Llama-2-7b-chat-hf"
 # model_name = "facebook/opt-125m"
-# model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
+# model_name = "facebook/opt-1.3b"
+# model_name = "meta-llama/Llama-2-70b-chat-hf"
+# model_name = "microsoft/phi-1_5"
+# model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+
+model.eval()
+model = convert(model, layer_from=torch.nn.Linear, layer_to=any4)
+model = model.to(device)
+
+log_dir = "./logs/llama2-7b-any4-cluster"
+import json
+import os
+os.makedirs(log_dir, exist_ok=True)
 
 # instantiate an LM subclass that takes initialized model and can run
 # - `Your_LM.loglikelihood()`
 # - `Your_LM.loglikelihood_rolling()`
 # - `Your_LM.generate_until()`
-lm_obj = lm_eval.models.huggingface.HFLM(pretrained=model_name, batch_size=16)
-lm_obj._model = convert(lm_obj.model, layer_from=torch.nn.Linear, layer_to=any4)
+lm_obj = lm_eval.models.huggingface.HFLM(pretrained=model, batch_size=16)#, parallelize=True)
+# lm_obj._model = convert(lm_obj.model, layer_from=torch.nn.Linear, layer_to=any4)
 
 # indexes all tasks from the `lm_eval/tasks` subdirectory.
 # Alternatively, you can set `TaskManager(include_path="path/to/my/custom/task/configs")`
@@ -28,6 +45,7 @@ results = lm_eval.simple_evaluate( # call simple_evaluate
     tasks=["social_iqa", "piqa"],
     num_fewshot=0,
     task_manager=task_manager,
+    # model_args={"parallelize": True},
 )
 
 print(results["results"])
