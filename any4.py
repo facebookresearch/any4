1b7756c209435557c53bf4643ab4ec6334a7ee5a
Mostafa Elhoushi <melhoushi@meta.com>
2024-06-11 14:03:55 +0000

    implement groupwise quant and dequant

--- any4.py
+++ any4.py
@@ -35,6 +35,75 @@ def convert(model: torch.nn.Module, layer_from: Type, layer_to: Callable, **kwar
     return model
 
 
+# Performs row-wise (k/reduction dimension-wise) int4 group
+# quantization on the m x k input tensor. Returns a tensor of the same
+# size with values quantized to [0, 2^n_bit - 1], along with scale and zero point
+# Reconstruction is bf16(int4_value) * scale + min_val
+def group_quantize_tensor(w_orig, n_bit, q_group_size=128):
+    w = w_orig.float()
+    assert q_group_size > 1
+    assert w.shape[-1] % q_group_size == 0
+    assert w.dim() == 2
+
+    to_quant = w.reshape(-1, q_group_size)
+    assert torch.isnan(to_quant).sum() == 0
+
+    max_val = to_quant.amax(dim=1, keepdim=True)
+    min_val = to_quant.amin(dim=1, keepdim=True)
+    max_int = 2**n_bit - 1
+    min_int = 0
+    scales = (max_val - min_val).clamp(min=1e-6) / max_int
+    assert torch.isnan(scales).sum() == 0
+
+    zeros = min_val + scales * (2 ** (n_bit - 1))
+    assert torch.isnan(zeros).sum() == 0
+
+    out = to_quant.sub(min_val).div(scales).round().clamp_(min_int, max_int)
+    assert torch.isnan(out).sum() == 0
+
+    out = out.to(dtype=torch.int32).reshape(w.shape)
+
+    # Scales and zeros for the same q-group should be contiguous, so we can
+    # load as a 32-bit word
+    scales = scales.view(w.shape[0], -1)
+    zeros = zeros.view(w.shape[0], -1)
+    scales_and_zeros = (
+        torch.cat(
+            [
+                scales.reshape(scales.size(0), scales.size(1), 1),
+                zeros.reshape(zeros.size(0), zeros.size(1), 1),
+            ],
+            2,
+        )
+        .transpose(0, 1)
+        .contiguous()
+    )
+
+    return out, scales_and_zeros.to(w_orig.dtype)
+
+
+# takes the scale or offset per each quantization group and reshapes/duplicates it to map
+# to the original matrix size
+def expand_q_groups(x, orig_size, q_group_size):
+    out = x.reshape(orig_size[0], orig_size[1] // q_group_size, 1)
+    out = out.expand(orig_size[0], orig_size[1] // q_group_size, q_group_size)
+    return out.contiguous().view(orig_size)
+
+
+def group_dequantize_tensor(x, scales_and_zeros, n_bit, q_group_size=128):
+    scales = scales_and_zeros.transpose(0, 1)[:, :, 0]
+    zeros = scales_and_zeros.transpose(0, 1)[:, :, 1]
+
+    scales = expand_q_groups(scales, x.size(), q_group_size)
+    zeros = expand_q_groups(zeros, x.size(), q_group_size)
+
+    min_val = zeros - scales * (2 ** (n_bit - 1))
+
+    out = x.bfloat16() * scales + min_val
+
+    return out
+
+
 def anyq(module: torch.nn.Module, n_bits: int=4, n_rows: int= 2048, n_iter: int=20):
     out_features, in_features = module.weight.shape
     n_rows = n_rows if n_rows else out_features
