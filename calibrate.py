6052c2f33a62af7c9dd717dbd1c15d768d491b47
Mostafa Elhoushi <melhoushi@meta.com>
2024-07-02 17:42:10 +0000

    record mean activations

--- calibrate.py
+++ calibrate.py
@@ -1,26 +1,30 @@
-from typing import Type
+from typing import Iterable, List, Tuple, Type
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-activation = {}
+layer_to_mean_activations = {}
 
-def get_activation(name):
+def get_mean_activations(name):
     def hook(model, input, output):
-        activation[name] = output.detach()
+        if isinstance(input, (List, Tuple)):
+            input = input[0]
+        # take the mean along all dimensions except the embedding dimension (which is the last dimension)
+        mean_activation = input.detach().mean(dim=torch.arange(input.ndim - 1).tolist())
+        layer_to_mean_activations[name] = mean_activation
     return hook
 
 def register_forward_hook(model: torch.nn.Module, layer_type: Type = torch.nn.Linear, **kwargs):
     for name, module in model.named_modules():
         if isinstance(module, (layer_type)):
-            module.register_forward_hook(get_activation(name))
+            module.register_forward_hook(get_mean_activations(name))
 
     return model
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# model_name = "meta-llama/Meta-Llama-3-8B"
-model_name = "facebook/opt-125m"
+model_name = "meta-llama/Meta-Llama-3-8B"
+# model_name = "facebook/opt-125m"
 
 model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -32,4 +36,4 @@ prompt = "Hello, my name is"
 inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
 model(inputs)
 
-print(activation)
+print(layer_to_mean_activations)
