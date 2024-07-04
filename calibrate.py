804529bc6b07c281d7674ee14707166d38de1789
Mostafa Elhoushi <melhoushi@meta.com>
2024-07-04 16:56:23 +0000

    add option to calibrate dataset + save to pickle

--- calibrate.py
+++ calibrate.py
@@ -1,7 +1,10 @@
-from typing import Iterable, List, Tuple, Type
+from typing import List, Tuple, Type
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+import pickle
+from pathlib import Path
 
 layer_to_mean_activations = {}
 
@@ -32,8 +35,31 @@ model.eval()
 
 register_forward_hook(model)
 
-prompt = "Hello, my name is"
-inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
-model(inputs)
+prompt =    """This is a diverse prompt that contains:
+            - Fiction: "Once upon a time, a girl named Alice was living alone on an island. One day, she met a wizard ..."
+            - News: "The United Nations held its General Assembly meeting this year amid multiple world crises and wars. In his speech, the General Secretary called for ..."
+            - Code: `public static void main(String[] args) {\n System.out.println("Hello world!);\n}`
+            - Math: (5.2 + 2.7) / 0.6 - 1.9 * 2.2 =
+            - Facts: "The capital of Egypt is Cairo. It is the largest city in the region and is home to..."
+            """
+
+if False:
+    dataset = "bigcode/the-stack"
+    num_samples = 200
+    split = "train"
+    field = "content"
+    for idx, sample in enumerate(load_dataset(dataset, split=split, streaming=True)):
+        if idx >= num_samples:
+            break
+        inputs = tokenizer.encode(sample[field], return_tensors="pt").to(device)
+        model(inputs)
+else:
+    inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
+    model(inputs)
 
 print(layer_to_mean_activations)
+
+pickle_name = Path(f"./profiles/{model_name}_prompt.pickle")
+pickle_name.parent.mkdir(parents=True, exist_ok=True)
+with open(pickle_name, 'wb') as handle:
+    pickle.dump(layer_to_mean_activations, handle, protocol=pickle.HIGHEST_PROTOCOL)
