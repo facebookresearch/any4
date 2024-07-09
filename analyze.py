87a91fbdac44a037381945f24019fc771503b5d1
Mostafa Elhoushi <melhoushi@meta.com>
2024-07-09 18:32:26 +0000

    support multiple layers in args

--- analyze.py
+++ analyze.py
@@ -13,7 +13,7 @@ from utils import CustomJSONEncoder
 
 def main(
     model_name: str,
-    layer: int,
+    layers: List[int],
     sub_layer: str,
     row: int,
     quant_args: Dict,
@@ -45,39 +45,46 @@ def main(
         model_args["quantization_config"] = bnb_config
 
     model = transformers.AutoModelForCausalLM.from_pretrained(model_name, **model_args).to(device)
+    modules = []
+    names = []
     for name, module in model.named_modules():
-        if name == f"model.layers.{layer}.{sub_layer}":
-            break
+        for layer in layers:
+            if name == f"model.layers.{layer}.{sub_layer}":
+                modules.append(module)
+                names.append(name)
 
-    # Store the weight
-    w = module.weight.data.clone()
+    for name, module in zip(names, modules):
+        print(f"{name}...", end="", flush=True)
+        # Store the weight
+        w = module.weight.data.clone()
 
-    # Plot original weight distribution
-    counts, bins = torch.histogram(module.weight.data[row].float().cpu(), bins=40)
-    plt.figure()
-    plt.hist(bins[:-1], bins=bins, weights=counts.float().numpy())
-    plt.show()
-    plt.savefig(log_dir / "w.png")
+        # Plot original weight distribution
+        counts, bins = torch.histogram(module.weight.data[row].float().cpu(), bins=40)
+        plt.figure()
+        plt.hist(bins[:-1], bins=bins, weights=counts.float().numpy())
+        plt.show()
+        plt.savefig(log_dir / "w.png")
 
-    # Apply our quantization algorithms
-    if quant_method:
-        module = convert(module, layer_from=torch.nn.Linear, layer_to=quant_method, **quant_args)
+        # Apply our quantization algorithms
+        if quant_method:
+            module = convert(module, layer_from=torch.nn.Linear, layer_to=quant_method, **quant_args)
 
-    # Get mean square error
-    wq = module.weight.data
-    mse = torch.mean((w - wq)**2)
-    print(f"Mean Square Error: {mse}")
+        # Get mean square error
+        wq = module.weight.data
+        mse = torch.mean((w - wq)**2)
+        print(f"\tMean Square Error: {mse}")
+        # TODO: Log to CSV file
 
-    # Overlay quantized values
-    for wq_val in module.weight.data[row].unique().float().cpu():
-        plt.axvline(x=wq_val, color="b", linestyle="--")
+        # Overlay quantized values
+        for wq_val in module.weight.data[row].unique().float().cpu():
+            plt.axvline(x=wq_val, color="b", linestyle="--")
 
-    # Plot reconstructed weight distribution
-    counts, bins = torch.histogram(module.weight.data[row].float().cpu(), bins=40)
-    plt.figure()
-    plt.hist(bins[:-1], bins=bins, weights=counts.float().numpy())
-    plt.show()
-    plt.savefig(log_dir / "w_reconstructed.png")
+        # Plot reconstructed weight distribution
+        counts, bins = torch.histogram(module.weight.data[row].float().cpu(), bins=40)
+        plt.figure()
+        plt.hist(bins[:-1], bins=bins, weights=counts.float().numpy())
+        plt.show()
+        plt.savefig(log_dir / "w_reconstructed.png")
 
 
 if __name__ == '__main__':
@@ -97,7 +104,7 @@ if __name__ == '__main__':
     parser.add_argument("--bnb-args", type=str, help="Comma separated string args to pass to BitsAndBytes quantization config.")
     parser.add_argument("--device", type=str, default=default_device, help="Device to use.")
     parser.add_argument("--log-dir", type=Path, default="./logs", help="Directory to log to.")
-    parser.add_argument("--layer", type=int, default=0, help="Transformer layer to analyze")
+    parser.add_argument("--layers", type=int, nargs="+", default=0, help="Transformer layer to analyze")
     parser.add_argument("--sub-layer", type=str, choices=["self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj", "self_attn.o_proj", "mlp.gate_proj", "mlp.up_proj", "mlp.down_proj"], default="self_attn.q_proj", help="Linear module within a transformer layer to analyze.")
     parser.add_argument("--row", type=int, default=0, help="Row of weight matrix to analyze.")
 
@@ -110,4 +117,4 @@ if __name__ == '__main__':
     bnb_args = None if not args.bnb_args else simple_parse_args_string(args.bnb_args)
 
     # Run Evaluation
-    main(model_name=args.model_name, layer=args.layer, sub_layer=args.sub_layer, row=args.row, model_args=model_args, quant_method=quant_method, quant_args=quant_args, device=args.device, log_dir=args.log_dir, bnb_args=bnb_args)
+    main(model_name=args.model_name, layers=args.layers, sub_layer=args.sub_layer, row=args.row, model_args=model_args, quant_method=quant_method, quant_args=quant_args, device=args.device, log_dir=args.log_dir, bnb_args=bnb_args)
