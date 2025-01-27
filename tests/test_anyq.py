667e32f76e6b52b773f28dc17dc1bdb94a20ae0b
Mostafa Elhoushi <melhoushi@meta.com>
2025-01-27 05:37:44 +0000

    successful anyq test

---
+++ tests/test_anyq.py
@@ -0,0 +1,30 @@
+import unittest
+import torch
+from parameterized import parameterized
+import itertools
+import numpy as np
+
+import any4
+from modules import QLinear
+import tinygemm
+import tinygemm.utils
+
+class TestAnyQ(unittest.TestCase):
+    # TODO: sweep over the arguments of anyq()
+    def test_quantize_dequantize(self, M=4096, N=4096, group_size=64, n_bit=4, parallelize=True, dtype=torch.float32):
+        device = "cuda"
+        new_grouping = False
+        assert group_size % 2**n_bit == 0, f"This test case assumes that group_size is a multiple of 2**n_bit, but instead we got group_size={group_size}, n_bit={n_bit}."
+        assert N % group_size == 0, f"This test case assumes that number of elements per row is a multiple of group_size, but instead we got N={N}, group_size={group_size}."
+
+        # if the weights are equally spaced and have the same the number of samples as 2**n_bit, quantizing and dequantizing should be exact
+        a, b = np.random.normal(), np.random.normal()
+        w_min, w_max = min(a, b), max(a, b)
+        w_vals = torch.linspace(start=w_min, end=w_max, steps=2**n_bit, dtype=dtype, device=device)
+        w_indices = torch.stack([torch.randperm(2**n_bit) for _ in range(M * N // 2**n_bit)]).view(M, N)
+        w = w_vals[w_indices]
+
+        wq, lut, scales_and_zeros = any4.anyq_quantize(w, n_bit=n_bit, q_group_size=group_size, new_grouping=new_grouping, parallelize=parallelize)
+        wdeq = any4.anyq_dequantize(wq, lut, scales_and_zeros=scales_and_zeros, n_bit=n_bit, q_group_size=group_size, new_grouping=new_grouping)
+
+        torch.testing.assert_close(w, wdeq)
