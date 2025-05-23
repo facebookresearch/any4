2a651bcbb7c3baaf12d950e2699619b9ea6b2910
Mostafa Elhoushi <m.elhoushi@ieee.org>
2025-05-23 00:27:00 +0000

    add reconstruct test

--- tests/test_intq.py
+++ tests/test_intq.py
@@ -43,6 +43,33 @@ class TestIntQ(unittest.TestCase):
         torch.testing.assert_close(w, wdeq)
 
     @parameterized.expand([
+        (M, N, group_size, n_bit, unsigned)
+        for M in [1, 4, 8, 24]
+        for N in [256, 1024, 2048]
+        for group_size in [32, 64, 128]
+        for n_bit in [2, 3, 4, 6, 8]
+        for unsigned in [True, False]
+        if group_size % 2**n_bit == 0 and N % group_size == 0  # Conditions to filter combinations
+    ])
+    def test_reconstruct(self, M=4096, N=4096, group_size=64, n_bit=4, unsigned=True, dtype=torch.float32):
+        device = "cuda"
+        new_grouping = False
+        zero_point = False
+        assert group_size % 2**n_bit == 0, f"This test case assumes that group_size is a multiple of 2**n_bit, but instead we got group_size={group_size}, n_bit={n_bit}."
+        assert N % group_size == 0, f"This test case assumes that number of elements per row is a multiple of group_size, but instead we got N={N}, group_size={group_size}."
+
+        # if the weights are equally spaced and have the same the number of samples as 2**n_bit, quantizing and dequantizing should be exact
+        a, b = np.random.normal(), np.random.normal()
+        w_min, w_max = min(a, b), max(a, b)
+        w_vals = torch.linspace(start=w_min, end=w_max, steps=2**n_bit, dtype=dtype, device=device)
+        w_indices = torch.stack([torch.randperm(2**n_bit) for _ in range(M * N // 2**n_bit)]).view(M, N)
+        w = w_vals[w_indices]
+
+        wdeq = any4.intq_reconstruct(w, n_bit=n_bit, q_group_size=group_size, dtype=dtype, new_grouping=new_grouping, unsigned=unsigned, zero_point=zero_point)
+
+        torch.testing.assert_close(w, wdeq)
+
+    @parameterized.expand([
         (M, N, group_size, n_bit, dtype)
         for M in [1, 4, 8, 24]
         for N in [256, 1024, 2048]
