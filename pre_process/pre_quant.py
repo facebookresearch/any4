61c18ea68b53224b7e66769c3606f04e8b7b4b4d
Mostafa Elhoushi <m.elhoushi@ieee.org>
2025-05-24 22:56:21 +0000

    support numeric type arg for awq

--- pre_process/pre_quant.py
+++ pre_process/pre_quant.py
@@ -6,12 +6,13 @@ from .awq.pre_quant import run_awq, apply_awq
 
 # TODO: use our intq and adapt it to AWQ's integer quantization
 # TODO: use our calibration function and adapt it to AwQ's calibration function
-def awq(model, tokenizer, n_bit = 4, q_group_size=128, zero_point=True, **kwargs):
+def awq(model, tokenizer, n_bit = 4, q_group_size=128, zero_point=True, numeric_type="int", **kwargs):
     orig_device_map = infer_auto_device_map(model)
 
     q_config = {
         "zero_point": zero_point,
         "q_group_size": q_group_size,
+        "numeric_type": numeric_type,
     }
     awq_results = run_awq(
         model,
